786e1431948798699f6f614ee48d05a230cdafb2
Robert Buruleanu <robertionut95b@gmail.com>
2022-02-18 22:06:39 +0200

    fix apollo client url

--- lib/apollo/apolloClient.ts
+++ lib/apollo/apolloClient.ts
@@ -6,15 +6,12 @@ import {
 } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 
-const API_GRAPHQL_URL =
-  process.env.API_GRAPHQL_URL || 'http://localhost:3000/api/graphql';
-
 const errorLink = onError(({ graphQLErrors }) => {
   if (graphQLErrors) graphQLErrors.map(({ message }) => console.error(message));
 });
 
 const httpLink = new HttpLink({
-  uri: API_GRAPHQL_URL,
+  uri: process.env.API_GRAPHQL_URL,
 });
 
 const apolloClient = new ApolloClient({
