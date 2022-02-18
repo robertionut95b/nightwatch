d284be25945b3c756cc1940ce3f0ffc2c9409aaf
Robert Buruleanu <robertionut95b@gmail.com>
2022-02-18 21:50:46 +0200

    apollo client url changes

--- lib/apollo/apolloClient.ts
+++ lib/apollo/apolloClient.ts
@@ -1,23 +1,25 @@
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 
-const API_URL = process.env.API_URL || 'localhost';
-const API_PORT = process.env.API_PORT || 3000;
-const API_GRAPHQL_URL = process.env.API_GRAPHQL_URL || 'api/graphql';
-const USE_SSL = process.env.USE_SSL || false;
+const API_GRAPHQL_URL =
+  process.env.API_GRAPHQL_URL || 'http://localhost:3000/api/graphql';
 
 const errorLink = onError(({ graphQLErrors }) => {
   if (graphQLErrors) graphQLErrors.map(({ message }) => console.error(message));
 });
 
 const httpLink = new HttpLink({
-  uri: `${USE_SSL === null || USE_SSL === undefined || USE_SSL === false ? 'https' : 'http'}://${API_URL}:${API_PORT}/${API_GRAPHQL_URL}`,
+  uri: API_GRAPHQL_URL,
 });
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   link: ApolloLink.from([errorLink, httpLink]),
-  // uri: `${USE_SSL === null || USE_SSL === undefined || USE_SSL === false ? "https" : "http"}://${API_URL}:${API_PORT}/${API_GRAPHQL_URL}`
 });
 
 export default apolloClient;
