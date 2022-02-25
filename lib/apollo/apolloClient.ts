d7b5cd7b3c0d7db17b46f772700f88fa3ff172b1
Robert Buruleanu <robertionut95b@gmail.com>
2022-02-25 17:36:00 +0200

    add apollo persisted queries

--- lib/apollo/apolloClient.ts
+++ lib/apollo/apolloClient.ts
@@ -1,8 +1,14 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries';
+import { sha256 } from 'crypto-hash';
+
+const linkChain = createPersistedQueryLink({ sha256 }).concat(
+  new HttpLink({ uri: process.env.API_GRAPHQL_URL }),
+);
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: process.env.API_GRAPHQL_URL,
+  link: linkChain,
 });
 
 export default apolloClient;
